4995a08f4b24eed9df28929e23298d76f28af8f1
ahlam izz <dialadista@hotmail.com>
2018-06-20 16:36:12 +0300

    we create sheet for HomePage and we do several tests on HomePage for logo, right header, footer, ShipTo US and ShipTo internationally and for My Account for US and CAD users and for international users

--- src/com/generic/page/HomePage.java
+++ src/com/generic/page/HomePage.java
@@ -16,6 +16,7 @@ import org.apache.commons.io.FileUtils;
 import com.generic.selector.CartSelectors;
 import com.generic.selector.CheckOutSelectors;
 import com.generic.selector.HomePageSelectors;
+import com.generic.selector.MyAccount_Selectors;
 import com.generic.setup.EnvironmentFiles;
 import com.generic.setup.LoggingMsg;
 import com.generic.setup.SelTestCase;
@@ -117,8 +118,118 @@ public class HomePage extends SelTestCase {
 		
 		getCurrentFunctionName(false);
 		return !diff.hasDiff();
+	}	
+	
+	public static Boolean checkMyAccountBttn(String country) throws Exception {
+		getCurrentFunctionName(true);
+		List<String> subStrArr = new ArrayList<String>();
+		List<String> valuesArr = new ArrayList<String>();
+		logs.debug(MessageFormat.format(LoggingMsg.GET_ELEMENT_BY_LOCATOR, HomePageSelectors.MyAccount));
+		subStrArr.add(HomePageSelectors.MyAccount);
+		Boolean myAccountBtn = false;
+		if (country.contains("United states")) {
+			myAccountBtn = SelectorUtil.isDisplayed(subStrArr);
+		}
+		else {
+			myAccountBtn =SelectorUtil.isNotDisplayed(subStrArr);
+		}
+		//SelectorUtil.initializeSelectorsAndDoActions(subStrArr, valuesArr);
+		//email = SelectorUtil.textValue.get();
+	//	logs.debug("The user email found in my account page: " + email);
+		getCurrentFunctionName(false);
+		return myAccountBtn;
+	}
+	public static Boolean MyAccountBttn(String email) throws Exception {
+		getCurrentFunctionName(true);
+		List<String> subStrArr = new ArrayList<String>();
+		List<String> valuesArr = new ArrayList<String>();
+		logs.debug(MessageFormat.format(LoggingMsg.GET_ELEMENT_BY_LOCATOR, HomePageSelectors.MyAccount));
+		subStrArr.add(HomePageSelectors.MyAccount);
+		valuesArr.add("");
+		
+		SelectorUtil.initializeSelectorsAndDoActions(subStrArr, valuesArr);
+		//email = SelectorUtil.textValue.get();
+	//	logs.debug("The user email found in my account page: " + email);
+		getCurrentFunctionName(false);
+		return SelectorUtil.isDisplayed(subStrArr);
+	}
+	public static boolean verifyLogo(String baseline) throws Exception {
+		List<String> subStrArr = new ArrayList<String>();
+		List<String> valuesArr = new ArrayList<String>();
+		getCurrentFunctionName(true);
+		logs.debug(MessageFormat.format(LoggingMsg.TYPING_ELEMENT_VALUE, "guest mail", baseline));
+		subStrArr.add(HomePageSelectors.logo);
+		valuesArr.add("VisualTesting");
+		SelectorUtil.initializeSelectorsAndDoActions(subStrArr, valuesArr);
+		String imagePath = EnvironmentFiles.getLogFilePath() + "/" + baseline+"_actual.png";
+		BufferedImage actualImage = SelectorUtil.screenShot.get().getImage();
+		
+		ImageIO.write(SelectorUtil.screenShot.get().getImage(),"PNG",new File(imagePath));
+		logs.debug("IMAGE:<br><a target=\"_blank\" href="+ baseline+"_actual.png"+"><img src=" + baseline+"_actual.png"+" alt=" + baseline+"_actual.png"+" style=\"width:150px\"></a>");
+		
+		String BaseImagePath = EnvironmentFiles.getVisualTestingAssetsPath() + "/" + baseline+".png";
+		
+		BufferedImage expectedImage = ImageIO.read(new File(BaseImagePath));
+        
+		ImageDiffer imgDiff = new ImageDiffer();
+        ImageDiff diff = imgDiff.makeDiff(actualImage, expectedImage);
+		
+		getCurrentFunctionName(false);
+		return !diff.hasDiff();
+	}
+	// by Diala
+	
+	public static void updateBaselineForRH(String baseline) throws Exception {
+		getCurrentFunctionName(true);
+		String VTAs =EnvironmentFiles.getVisualTestingAssetsPath();
+		String baselineAbsPath = VTAs + "/" + baseline+ ".png";
+		String logs_dir = EnvironmentFiles.getLogFilePath();	
+    	File baseLineFile = new File(baselineAbsPath);
+		FileUtils.copyFileToDirectory(baseLineFile, Paths.get(logs_dir).toFile());
+		String baselinePathInLogs =  logs_dir + "/" + baseline + ".png";
+		logs.debug("IMAGE:<br><a target=\"_blank\" href="+ baseline + ".png"+"><img src=" + baseline + ".png"+" alt=" + baseline + ".png"+" style=\"width:150px\"></a>");
+		getCurrentFunctionName(false);
+	}
+	public static boolean verifyRightHeader(String baseline) throws Exception {
+		List<String> subStrArr = new ArrayList<String>();
+		List<String> valuesArr = new ArrayList<String>();
+		getCurrentFunctionName(true);
+		logs.debug(MessageFormat.format(LoggingMsg.TYPING_ELEMENT_VALUE, "guest mail", baseline));
+		subStrArr.add(HomePageSelectors.RightHeader);
+		valuesArr.add("VisualTesting");
+		SelectorUtil.initializeSelectorsAndDoActions(subStrArr, valuesArr);
+		String imagePath = EnvironmentFiles.getLogFilePath() + "/" + baseline+"_actual.png";
+		BufferedImage actualImage = SelectorUtil.screenShot.get().getImage();
+		
+		ImageIO.write(SelectorUtil.screenShot.get().getImage(),"PNG",new File(imagePath));
+		logs.debug("IMAGE:<br><a target=\"_blank\" href="+ baseline+"_actual.png"+"><img src=" + baseline+"_actual.png"+" alt=" + baseline+"_actual.png"+" style=\"width:150px\"></a>");
+		
+		String BaseImagePath = EnvironmentFiles.getVisualTestingAssetsPath() + "/" + baseline+".png";
+		
+		BufferedImage expectedImage = ImageIO.read(new File(BaseImagePath));
+        
+		ImageDiffer imgDiff = new ImageDiffer();
+        ImageDiff diff = imgDiff.makeDiff(actualImage, expectedImage);
+		
+		getCurrentFunctionName(false);
+		return !diff.hasDiff();
 	}
 	
+
+	
+	public static String ShipTo() throws Exception {
+		getCurrentFunctionName(true);
+		List<String> subStrArr = new ArrayList<String>();
+		List<String> valuesArr = new ArrayList<String>();
+		logs.debug(MessageFormat.format(LoggingMsg.GET_ELEMENT_BY_LOCATOR, HomePageSelectors.ShipTo));
+		subStrArr.add(HomePageSelectors.ShipTo);
+	
+		
+		//email = SelectorUtil.textValue.get();
+	//	logs.debug("The user email found in my account page: " + email);
+		getCurrentFunctionName(false);
+		return SelectorUtil.getAttr(subStrArr, "src");
+	}
 	public static boolean verifyFooter(String baseline) throws Exception {
 		List<String> subStrArr = new ArrayList<String>();
 		List<String> valuesArr = new ArrayList<String>();
@@ -169,6 +280,9 @@ public class HomePage extends SelTestCase {
 		return !diff.hasDiff();
 	}
 
+	
+	
+	
 	public static void changeCountry(String country) throws Exception{
 		clickShipToBtn();
 		selectCountry(country);
--- src/com/generic/selector/HomePageSelectors.java
+++ src/com/generic/selector/HomePageSelectors.java
@@ -4,13 +4,17 @@ package com.generic.selector;
 public class HomePageSelectors
 {
 	public static final String header = "js-mainHeader";
-	public static final String footer = "footer__left col-xs-12 col-sm-12 col-md-9";
+	public static final String footer = "css,#main-footer>#footer-elements";
 	public static final String body = "Loyal Customers";
 	public static final String subcriptionCloseIcon= "css,.popup-dialog>.popup-content>div>div>div>a";
 	public static final String shipToIcon= "intl-flag";
 	public static final String selectCountry= "country";
 	public static final String updateCountryLink= "update-country-link";
 	public static final String selectCurrency= "currencyCode";
-	
+	public static final String logo = "css,#main-header-logo>a";
+	public static final String MyAccount ="my-account"; 
+	public static final String RightHeader ="css,#right-header";
+	public static final String ShipTo ="intl-flag";
+
 	
 }
